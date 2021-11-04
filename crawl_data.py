7abf99b1499d7a494c646297d7ec80f2ba718526
TranBaoDuy <duycarry123@gmail.com>
2021-11-04 21:17:44 +0700

    add more data

--- crawl_data.py
+++ crawl_data.py
@@ -23,54 +23,58 @@ chrome_options.add_argument('--disable-dev-shm-usage')
 browser = webdriver.Chrome();
 
 # BASE_URL
-BASE_URL = 'https://tiki.vn/bestsellers-2020/dien-thoai-may-tinh-bang/c1789'
-# Headers 
+BASE_URL = [
+    'https://tiki.vn/bestsellers-2020/dien-thoai-may-tinh-bang/c1789',
+    'https://tiki.vn/bestsellers-2020/dien-thoai-may-tinh-bang/c1789?p=2',
+    'https://tiki.vn/bestsellers-2020/dien-thoai-may-tinh-bang/c1789?p=3',
+    'https://tiki.vn/bestsellers-2020/dien-thoai-may-tinh-bang/c1789?p=4'
+]
 
-
-# Open website
-browser.get(BASE_URL)
-
-sleep(2)
-
-# Get page source
-page_source = BeautifulSoup(browser.page_source)
-
-# Get Link of Product
-link_phone_list = []
-for link in page_source.select('p.title a[href]'):
-    link_phone_list.append(link['href'])
-
-# Get Product id of each Product with re
-product_id_list = []
-for i in range(len(link_phone_list)):
-    txt = link_phone_list[i]
-    product_id = re.findall("p\d{1,10}", txt)
-    product_id = re.sub("p","", product_id[0])
-    product_id_list.append(product_id)
-
-
-# Get data from api
 comment_list = []
-def getData(url):    
-    header = {'user-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36"}
-    response = requests.get(url,headers=header)
-    data = response.json()['data'] 
-    
-    for i in range(len(data)):
-        if data[i]['content']:            
-            comment_list.append({"content":data[i]['content'],"rating":data[i]['rating']})
-    print("Is crawl:", len(comment_list) , " comments")
+for i in range(len(BASE_URL)):
+    # Open website
+    browser.get(BASE_URL[i])
 
-# Loop product id
-for i in range(len(product_id_list)):
-    product_id = product_id_list[i]
-    if len(comment_list) < 10000:
+    sleep(2)
+
+    # Get page source
+    page_source = BeautifulSoup(browser.page_source)
+
+    # Get Link of Product
+    link_phone_list = []
+    for link in page_source.select('p.title a[href]'):
+        link_phone_list.append(link['href'])
+
+    # Get Product id of each Product with re
+    product_id_list = []
+    for i in range(len(link_phone_list)):
+        txt = link_phone_list[i]
+        product_id = re.findall("p\d{1,10}", txt)
+        product_id = re.sub("p","", product_id[0])
+        product_id_list.append(product_id)
+
+    # Get data from api
+    def getData(url):    
+        print("API:", url)
+        header = {'user-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36"}
+        response = requests.get(url,headers=header)
+        if response.status_code == 200:
+            data = response.json()['data']         
+            for i in range(len(data)):
+                if (data[i]['content'] and data[i]['rating']):                      
+                    comment_list.append({"content":data[i]['content'],"rating":data[i]['rating']})
+            print("Is crawl:", len(comment_list) , " comments")
+        else:
+            print("Time out connect")
+
+    # Loop product id
+    for i in range(len(product_id_list)):
+        print("\n================================================")
+        product_id = product_id_list[i]     
         print("Crawling Product Id:", product_id)
-        api_url = f"https://tiki.vn/api/v2/reviews?limit=1000&include=comments,contribute_info&sort=score%7Cdesc,id%7Cdesc,stars%7Call&page=1&product_id={product_id}"
+        api_url = f"https://tiki.vn/api/v2/reviews?limit=500&include=comments,contribute_info&sort=score%7Cdesc,id%7Cdesc,stars%7Call&page=1&product_id={product_id}"
         getData(api_url)
-    else:
-        print("Done")
-        break
+      
 
 # Save data into file
 with open('data_coment_tiki.json', 'w', encoding='utf-8') as file:
