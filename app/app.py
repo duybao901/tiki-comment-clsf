eae7031b48d52235de2d8ede612c64a4a58c05e7
TranBaoDuy <duycarry123@gmail.com>
2021-11-11 14:35:58 +0700

    update interface

--- app/app.py
+++ app/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__, template_folder='templates')
 def prediction():
     # Lấy dữ liệu người dụng nhập vào qua phương thức GET
     comment = request.args.get('comment')
-    return render_template('home.html', result = 'Bình luận của bạn đạt '+ comment +' sao')
+    return render_template('home.html', result =comment)
 
 
 # Trang chủ dẩn đến home
