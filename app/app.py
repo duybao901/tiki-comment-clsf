716e8d67929a44fce15251a2deb9dae329c1593b
Lee-TB <86998194+Lee-TB@users.noreply.github.com>
2021-11-11 12:50:07 +0700

    Add: web app flask

---
+++ app/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, render_template
+
+# Khởi tạo server backend
+app = Flask(__name__, template_folder='templates')
+
+# Đường dẫn chạy mô hình phân lớp
+@app.route('/prediction', methods=['GET'])
+def prediction():
+    # Lấy dữ liệu người dụng nhập vào qua phương thức GET
+    comment = request.args.get('comment')
+    return render_template('home.html', result = 'Bình luận của bạn đạt '+ comment +' sao')
+
+
+# Trang chủ dẩn đến home
+@app.route('/')
+def home():
+    return render_template('home.html') # Render home.html
+
+# Start server
+if __name__ == '__main__':
+    app.run(debug=True)
